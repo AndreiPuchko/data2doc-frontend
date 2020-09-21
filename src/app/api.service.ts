1b78f45a3946941a94e876954273c75fa23a5511
zzvillager <penta@penta.by>
2020-09-21 16:18:29 -0700

    done file exchanging

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
 
@@ -11,8 +11,12 @@ export class ApiService {
   file_data: any;
   constructor(private http: HttpClient) {}
 
+  public downloadResultFile(key): any {
+		return this.http.get(this.apiURL+"result/"+key, {responseType: 'blob'});
+  }
+
+
   public downloadSampleFileContent(filetype:string): any {
-		// return this.http.get(this.apiURL+"samplefilecontent/"+filetype, {responseType: 'text'});
 		return this.http.get(this.apiURL+"samplefile/"+filetype, {responseType: 'blob'});
   }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,40 +15,48 @@ export class AppComponent {
   baseUrl = environment.baseUrl;
 
   constructor(private apiService: ApiService,private http: HttpClient){}
-  
-  dropFile(file_object:File) {
-    var fileType;
-    fileType = file_object.name.slice(-4).toLowerCase();
-    for (let i = 0; i < this.filesDz.length; i++){
-      if (fileType===this.filesDz[i].name.slice(-4).toLowerCase())
-        this.filesDz.splice(i,1);
-    }
-    this.filesDz.push(file_object)
-  }
-  
+
   ngOnInit(){
     this.downloadSampleFiles();
   }
 
-  data2doc(){
-    // this.filesDz[0].text().then(val=>alert(val));
+  sampleDocx(){
+    this.apiService.downloadSampleFileContent("docx").subscribe(data=>{
+      let blob = new File([data], "sample.docx");
+      let url = window.URL.createObjectURL(blob);
+      window.location.href = url;
+    })
+  }
+  sampleXlsx(){
+    this.apiService.downloadSampleFileContent("xlsx").subscribe(data=>{
+      let blob = new File([data], "sample.xlsx");
+      let url = window.URL.createObjectURL(blob);
+      window.location.href = url;
+    })
+  }
 
+
+  downloadResult(key){
+    this.apiService.downloadResultFile(key).subscribe(data => {
+        console.log("gogogo");
+        let blob = new File([data], "result.docx");
+        let url = window.URL.createObjectURL(blob);
+        window.location.href = url;
+      },error=>{console.log("result file downloading error")});
+  }
+
+  data2doc(){
     let fd: FormData = new FormData();
     for (let i = 0; i < this.filesDz.length; i++){
-      // console.log(this.filesDz[i].size);
-      // console.log(this.filesDz[i]);
       fd.append('file', this.filesDz[i], this.filesDz[i].name);
     }
-    // this.http.post(this.baseUrl+"data2doctest",fd).subscribe(data => {alert(123333)}, error => console.log("res") );
-    
-    // this.apiService.sendFiles(fd).subscribe(data => {alert(123)}, error => console.log("res") );
-    
     this.apiService.sendFiles(fd).subscribe(
       data => {
-                console.log('Sent successfully.'+data['content']);
+                console.log('files sent successfully');
+                this.downloadResult(data['key']);
               },
       error => {
-                console.log('The given data was invalid.');
+                console.log('failed files download');
               },
       () => {
             //
@@ -56,12 +64,21 @@ export class AppComponent {
     );    
     }
 
+    showSample(filetype:string){
+      this.apiService.downloadSampleFileContent(filetype).subscribe(data=>{
+        let blob = new File([data], "sample."+filetype);
+        let url = window.URL.createObjectURL(blob);
+        window.location.href = url;
+      })
+    }
+  
+
   dropSampleFileContent(fileContent:string,filetype:string){
     // console.log(atob(fileContent));
     let file_object = new File([fileContent], 'sample.'+filetype, { type: filetype, lastModified:Date.now()});
-    console.log(file_object.size);
     this.dropFile(file_object);
   }
+
   getSample(filetype:string){
     this.apiService.downloadSampleFileContent(filetype).subscribe(data =>this.dropSampleFileContent(data,filetype),error=> alert("Download sample file error!"));
   }
@@ -70,6 +87,16 @@ export class AppComponent {
     // this.getSample("txt");
     this.getSample("xlsx");
     this.getSample("docx");
+  }
+
+  dropFile(file_object:File) {
+    var fileType;
+    fileType = file_object.name.slice(-4).toLowerCase();
+    for (let i = 0; i < this.filesDz.length; i++){
+      if (fileType===this.filesDz[i].name.slice(-4).toLowerCase())
+        this.filesDz.splice(i,1);
+    }
+    this.filesDz.push(file_object)
   }
 
   onSelectDz(event) {
