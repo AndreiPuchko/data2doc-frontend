4bbde828676812730961fc2de561527ba973d29c
zzvillager <penta@penta.by>
2020-10-09 02:01:23 -0700

    material design done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,39 @@ import { NgModule } from '@angular/core';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './navbar/navbar.component';
 import { WaitComponent } from './wait/wait.component';
-import { HomeComponent } from './home/home.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import {MaterialModule} from './material.module';
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
     WaitComponent,
-    HomeComponent,
+
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     NgxDropzoneModule,
-    MaterialModule,
     BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatSpinner} from '@angular/material/progress-spinner'; 
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  constructor() {}
+
+}
